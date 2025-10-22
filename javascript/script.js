1b063fa4898c57f0d559fafa423e31ea4a8a998b
Gonzalo Balay Losada <gbalay@denodo.com>
2025-10-22 16:25:17 +0200

    adding first javascript

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+const applyButtons = document.querySelectorAll('.apply-button-job');
+
+applyButtons.forEach(applyButton => {
+    applyButton.addEventListener('click', () => {
+        alert('¡Gracias por tu interés! Serás redirigido a la página de aplicación.');
+        applyButton.textContent = 'Aplicando...';
+        applyButton.disabled = true;
+        //applyButton.style.cursor = 'not-allowed';
+        //applyButton.style.backgroundColor = '#6c757d'; // Cambia el color para indicar que está deshabilitado
+        applyButton.classList.add('is-applied');
+        // Aquí podrías agregar lógica adicional para redirigir al usuario o enviar datos
+    });
+});
