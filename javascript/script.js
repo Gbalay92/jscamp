c4d9f69de08cb4201e7dd888e744a80039145b29
Gonzalo Balay Losada <gbalay@denodo.com>
2025-10-28 18:01:34 +0100

    fix filters issue

--- javascript/script.js
+++ javascript/script.js
@@ -79,17 +79,12 @@ jobListings?.addEventListener('click', (event) => {
 
 const filterLocation = document.getElementById('filter-location');
 const msg = document.querySelector('#filter-message');
-const jobs = document.querySelectorAll('.job');
+
 
 filterLocation?.addEventListener('change',() => {
     const selectedLocation = filterLocation.value;
-    if (selectedLocation) {
-        msg.textContent = `Filtrando por ubicación: ${filterLocation.value}`;
-    } else {
-        msg.textContent = '';
-    }
 
-    
+    const jobs = document.querySelectorAll('.job');
     jobs.forEach(job => {
         /*console.log(job.dataset.modalidad);
         if (selectedLocation === '' || job.dataset.modalidad === selectedLocation) {
@@ -99,7 +94,7 @@ filterLocation?.addEventListener('change',() => {
         }*/
         const modalidad = job.getAttribute('data-modalidad');
         const isShown = selectedLocation === '' || modalidad === selectedLocation;
-        job.classList.toggle('is-hidden', !isShown); //aplica o quita la clase is-hidden
+        job.classList.toggle('is-hidden', isShown === false); //aplica o quita la clase is-hidden
         
     });
 
