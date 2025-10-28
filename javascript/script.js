d04039edc769e5d3fe4a802bcae176aba675902d
Gonzalo Balay Losada <gbalay@denodo.com>
2025-10-28 17:33:44 +0100

    fetching jobs from json

--- javascript/script.js
+++ javascript/script.js
@@ -104,3 +104,24 @@ filterLocation?.addEventListener('change',() => {
     });
 
 });
+
+
+//trying fetch to data.json
+fetch("../javascript/data.json") //fetch es asícrono
+    .then(response => response.json())
+    .then(jobs => {
+        jobs.forEach(job => {
+            const article = document.createElement('article');
+            article.classList.add('job');
+            article.dataset.modalidad = job.data.modalidad;
+            article.dataset.nivel = job.data.nivel;
+            article.dataset.technology = job.data.technology;
+            article.innerHTML = `
+                <h3>${job.title}</h3>
+                <p>Company: ${job.company}</p>
+                <p>Location: ${job.location}</p>
+                <p>${job.description}</p>
+                <button class="apply-button-job">Apply</button>`;
+            jobListings.appendChild(article);
+        });
+    });
