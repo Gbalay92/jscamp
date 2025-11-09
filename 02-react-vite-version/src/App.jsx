a1e1c321664bd6e4e270f6645d5c0e3677eff413
Gonzalo Balay Losada <gbalay@denodo.com>
2025-11-09 17:30:44 +0100

    finishing the pagination

--- 02-react-vite-version/src/App.jsx
+++ 02-react-vite-version/src/App.jsx
@@ -4,12 +4,19 @@ import { Footer } from './components/Footer.jsx'
 import { Pagination } from './components/Pagination.jsx'
 import { JobList } from './components/JobList.jsx'
 import { SearchFormSection } from './components/SearchFormSection.jsx'
+import jobsData from './data.json'
 
+const RESULTS_PER_PAGE = 5
 
 function App() {
   const [currentPage, setCurrentPage] = useState(1)
-
+  const totalPages = Math.ceil(jobsData.length / RESULTS_PER_PAGE)
   
+  const pagedResults = jobsData.slice(
+    (currentPage - 1) * RESULTS_PER_PAGE, //pagina 1: 0-4, pagina 2: 5-9
+    currentPage * RESULTS_PER_PAGE //pagina 1: 5, pagina 2: 10
+  )
+
 function handlePageChange(newPage) {
   console.log('Página cambiada a:', newPage)
   setCurrentPage(newPage)
@@ -20,9 +27,9 @@ function handlePageChange(newPage) {
       <main>
           <SearchFormSection />
           <section>
-            <JobList />
+            <JobList jobs={pagedResults} />
           </section>
-          <Pagination currentPage={currentPage} totalPages={7} onPageChange={handlePageChange} />
+          <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
       </main>
       <Footer />
     </>
--- 02-react-vite-version/src/components/JobList.jsx
+++ 02-react-vite-version/src/components/JobList.jsx
@@ -1,13 +1,12 @@
-import jobsData from '../data.json'
 import { JobCard } from "./JobCard.jsx"
 
-export function JobList() {
+export function JobList({jobs}) {
 
   return (
     <>
       <h2>Resultados de búsqueda</h2>
       <div className="listing-jobs">
-        {jobsData.map(job => (
+        {jobs.map(job => (
           <JobCard key={job.id} job={job} />
         ))}
       </div>
