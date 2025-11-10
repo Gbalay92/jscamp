061e5ead24ee0accbbb428362e41486882e29d42
Gonzalo Balay Losada <gbalay@denodo.com>
2025-11-10 16:14:05 +0100

    do filters work

--- 02-react-vite-version/src/App.jsx
+++ 02-react-vite-version/src/App.jsx
@@ -9,23 +9,53 @@ import jobsData from './data.json'
 const RESULTS_PER_PAGE = 5
 
 function App() {
+  const [filters, setFilters] = useState({
+    technology: '',
+    location: '',
+    experience: ''
+  })
+  const [textFilter, setTextFilter] = useState('')
   const [currentPage, setCurrentPage] = useState(1)
-  const totalPages = Math.ceil(jobsData.length / RESULTS_PER_PAGE)
+
+  const filteredJobs =  textFilter === '' ? jobsData : jobsData.filter((job) => {
+    return job.title.toLowerCase().includes(textFilter.toLowerCase())
+  })
   
-  const pagedResults = jobsData.slice(
+
+  const jobsAfterFilter = filteredJobs.filter((job) => {
+    const matchesTechnology = filters.technology ? job.data.technology.toLowerCase() === filters.technology.toLowerCase() : true
+    const matchesLocation = filters.location ? job.location.toLowerCase() === filters.location.toLowerCase() : true
+    const matchesExperience = filters.experience ? job.experience.toLowerCase() === filters.experience.toLowerCase() : true
+
+    return matchesTechnology && matchesLocation && matchesExperience
+  })
+
+  const pagedResults = jobsAfterFilter.slice(
     (currentPage - 1) * RESULTS_PER_PAGE, //pagina 1: 0-4, pagina 2: 5-9
     currentPage * RESULTS_PER_PAGE //pagina 1: 5, pagina 2: 10
   )
+  const totalPages = Math.ceil(jobsAfterFilter.length / RESULTS_PER_PAGE)
 
-function handlePageChange(newPage) {
-  console.log('Página cambiada a:', newPage)
-  setCurrentPage(newPage)
-}
+
+  function handlePageChange(newPage) {
+    setCurrentPage(newPage)
+  }
+
+  const handleSearch = (searchParams) => {
+    setFilters(searchParams)
+    setCurrentPage(1) // Reiniciar a la primera página al cambiar los filtros de búsqueda
+  }
+
+  const handleTextFilter = (newTextToFilter) => {
+    setTextFilter(newTextToFilter)
+    setCurrentPage(1) // Reiniciar a la primera página al cambiar el filtro de texto
+  }
+
   return (
     <>
       <Header />
       <main>
-          <SearchFormSection />
+          <SearchFormSection onSearch={handleSearch} onTextFilter={handleTextFilter} />
           <section>
             <JobList jobs={pagedResults} />
           </section>
--- 02-react-vite-version/src/components/SearchFormSection.jsx
+++ 02-react-vite-version/src/components/SearchFormSection.jsx
@@ -1,12 +1,36 @@
-export function SearchFormSection() {
+import { useId } from "react"
+
+export function SearchFormSection({onSearch, onTextFilter}) {
+    const searchInputText = useId()
+    const searchInputTecnology = useId()
+    const searchInputLocation = useId()
+    const searchInputExperience = useId()
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const formData = new FormData(event.target)
+        const filters = {
+            technology: formData.get(searchInputTecnology),
+            location: formData.get(searchInputLocation),
+            experience: formData.get(searchInputExperience)
+        }
+
+        onSearch(filters)
+    }
+
+    const handleTextChange = (event) => {
+        const inputValue = event.target.value
+        onTextFilter(inputValue)
+    }
+
   return (
     <>
         <section>
               <img src="./background.webp" alt="background"></img>
           <h1>Encuentra el trabajo de tus sueños</h1>
           <p>Únete a la comunidad más grande de desarrolladores y encuentra tu próxima oportunidad.</p>
-          <form id="empleos-search-form" role="search">
-              <div>
+          <form onSubmit={handleSubmit} id="empleos-search-form" role="search">
+              <div className="search-bar">
                   <svg xmlns="http://www.w3.org/2000/svg" 
                   width="24" height="24" viewBox="0 0 24 24" 
                   fill="none" stroke="currentColor" 
@@ -15,27 +39,28 @@ export function SearchFormSection() {
                   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                   <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
                   <path d="M21 21l-6 -6" /></svg>
-                  <input type="text" placeholder="Buscar empleos por título, habilidad o empresa" aria-label="Buscar empleos" id="search-input"/>
+                  <input name={searchInputText} type="text" placeholder="Buscar empleos por título, habilidad o empresa" aria-label="Buscar empleos" id="search-input"
+                    onChange={handleTextChange}/>
                   <button type="submit">Buscar</button>
               </div>
           
           
               <div className="search-filters">
-                  <select name="technology" id="filter-technology">
+                  <select name={searchInputTecnology} id="filter-technology">
                       <option value="">Tecnología</option>
                       <option value="javascript">JavaScript</option>
                       <option value="python">Python</option>
                       <option value="java">Java</option>
                       <option value="ruby">Ruby</option>
                   </select>
-                  <select name="location" id="filter-location">
+                  <select name={searchInputLocation} id="filter-location">
                       <option value="">Ubicación</option>
                       <option value="remoto">Remoto</option>
                       <option value="madrid">Madrid, España</option>
                       <option value="barcelona">Barcelona, España</option>
                       <option value="valencia">Valencia, España</option>
                   </select>
-                  <select name="experience" id="filter-experience">
+                  <select name={searchInputExperience} id="filter-experience">
                       <option value="">Nivel de experiencia</option>
                       <option value="junior">Junior</option>
                       <option value="mid">Mid</option>
