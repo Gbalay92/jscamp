0d9378565baf8bb1706d68f1a40aa2411d7de205
Gonzalo Balay Losada <gbalay@denodo.com>
2025-11-19 17:30:23 +0100

    Route component

--- 02-react-vite-version/src/App.jsx
+++ 02-react-vite-version/src/App.jsx
@@ -4,30 +4,21 @@ import { HomePage } from './pages/Home.jsx'
 import { SearchPage } from './pages/Search.jsx'
 import { NotFoundPage } from './pages/404.jsx'
 import { useRouter } from './hooks/useRouter.jsx'
+import { Route } from './components/Route.jsx'
 
 
 
 function App() {
-  const { currentPath } = useRouter()
 
-  
-  let page = null
-  if (currentPath === '/') {
-    page = <HomePage />
-  } else if (currentPath === '/search') {
-    page = <SearchPage />
-  } else {
-    page = <NotFoundPage />
+  return (
+      <>
+        <Header />
+          <Route path="/" component={HomePage} />
+          <Route path="/search" component={SearchPage} />
+          <Route path="*" component={NotFoundPage} />
+        <Footer />
+      </>
+    )
   }
-
-
-return (
-    <>
-      <Header />
-        {page}
-      <Footer />
-    </>
-  )
-}
 
 export default App
--- 02-react-vite-version/src/pages/Home.jsx
+++ 02-react-vite-version/src/pages/Home.jsx
@@ -1,11 +1,26 @@
 export function HomePage() {
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const { navigateTo } = useRouter()
+        const formData = new FormData(event.target)
+        const searchTerm = formData.get('search')
+
+
+        const url = searchTerm !== ''
+            ? '/search?text=' + encodeURIComponent(searchTerm)
+            : '/search'
+        
+        navigateTo(url)
+    }
+    
     return (
         <main>
             <section>
                 <img src="./background.webp" />
             <h1>Encuentra el trabajo de tus sueños</h1>
             <p>Únete a la comunidad más grande de desarrolladores y encuentra tu próxima oportunidad.</p>
-            <form role="search">
+            <form onSubmit={handleSubmit} role="search">
                 <div>
                     <svg xmlns="http://www.w3.org/2000/svg" 
                     width="24" height="24" viewBox="0 0 24 24" 
@@ -15,7 +30,7 @@ export function HomePage() {
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
                     <path d="M21 21l-6 -6" /></svg>
-                    <input type="text" placeholder="Buscar empleos por título, habilidad o empresa" aria-label="Buscar empleos" />
+                    <input name="search" type="text" placeholder="Buscar empleos por título, habilidad o empresa" aria-label="Buscar empleos" />
                     <button type="submit">Buscar</button>
                 </div>
             </form>
