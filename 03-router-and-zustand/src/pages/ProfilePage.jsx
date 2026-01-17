75ef179d0cfec98c4cd65c369614329e66e9e93d
Gonzalo Balay Losada <gbalay@denodo.com>
2026-01-17 18:57:57 +0100

    building profile Page

--- 03-router-and-zustand/src/pages/ProfilePage.jsx
+++ 03-router-and-zustand/src/pages/ProfilePage.jsx
@@ -1,9 +1,17 @@
-export default function ProfilePage() {
+import styles from './Profile.module.css';
+import profilePicture from '../assets/profilePicture.jpg';
+
+export default function ProfilePage( ) {
     return (
-        <>
-            <h1>Profile Page</h1>
-            
-        
-        </>
+        <div className={styles.ProfileContainer}>
+            <div className={styles.ProfileCard}>
+                <img src={profilePicture} alt="Profile" className={styles.ProfileImage} />
+                <h3>Example User</h3>
+                <div className={styles.ProfileData}>
+                    <p>Email: example@example.com</p>
+                    <p>Member since: January 2022</p>
+                </div>
+            </div>
+        </div>
     )
 }
