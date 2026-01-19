835d1a2c96521fd9e2ac52fd07659eda58c51def
Gonzalo Balay Losada <gbalay@denodo.com>
2026-01-19 16:26:02 +0100

    clear favorites at logout

--- 03-router-and-zustand/src/context/AuthContext.jsx
+++ 03-router-and-zustand/src/context/AuthContext.jsx
@@ -1,12 +1,17 @@
 import { createContext, useState, useContext } from "react";
+import { useFavoritesStore } from "../store/favoritesStore.jsx";
 
 export const AuthContext = createContext(null);
 
 export function AuthProvider({ children }) {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const { clearFavorites } = useFavoritesStore();
 
     const handleLogin = () => setIsLoggedIn(true);
-    const handleLogout = () => setIsLoggedIn(false);
+    const handleLogout = () => {
+        setIsLoggedIn(false);
+        clearFavorites();
+    };
 
     return (
         <AuthContext.Provider value={{ isLoggedIn, handleLogin, handleLogout }}>
