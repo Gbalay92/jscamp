c93ccd6041031e6802e891082f010ab0c70a24fb
Gonzalo Balay Losada <gbalay@denodo.com>
2026-01-21 15:54:20 +0100

    catch permissions

--- 04-node/manageFiles.js
+++ 04-node/manageFiles.js
@@ -1,10 +1,20 @@
 import { readFile, writeFile, mkdir } from 'node:fs/promises'
 import { join } from 'node:path'
 
+let content = ''
+if(process.permissions.has('fs-read', './file.txt')) {
+    content = await readFile('./file.txt', 'utf-8')
+} else {
+    throw new Error('No permission to read file.txt')
+}
 
-const content = await readFile('./file.txt', 'utf-8')
 
-const outputDir = join('outputDir', 'files', 'docs')
-await mkdir(outputDir, { recursive: true })
+if(!process.permissions.has('fs-write', './outputDir')) {
+    throw new Error('No permission to write to outputDir')
+} else {
+    const outputDir = join('outputDir', 'files', 'docs')
+    await mkdir(outputDir, { recursive: true })
 
-await writeFile(join(outputDir, 'file2.txt'), content.toUpperCase())
+    await writeFile(join(outputDir, 'file2.txt'), content.toUpperCase())
+}
+    
