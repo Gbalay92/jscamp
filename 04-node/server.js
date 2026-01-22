43ba8a1243f8f7c68720944ef1657d378106f47c
Gonzalo Balay Losada <gbalay@denodo.com>
2026-01-22 18:08:21 +0100

    use env variable to set port

--- 04-node/server.js
+++ 04-node/server.js
@@ -1,6 +1,6 @@
 import { createServer } from 'node:http'
 
-const port = 3000
+const port = process.env.PORT ?? 3000
 
 const server = createServer((req, res) => {
     console.log(`${req.method} ${req.url}`)
