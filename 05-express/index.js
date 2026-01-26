7f084be89522d56df0141bc64c039772cae2000a
Gonzalo Balay Losada <gbalay@denodo.com>
2026-01-26 18:31:16 +0100

    adding first middlewares

--- 05-express/index.js
+++ 05-express/index.js
@@ -4,9 +4,19 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 
-app.get('/', (req, res) => {
+app.get('/', prevHomeMiddleware, (req, res) => {
   res.send('Hello, Express!')
 })
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`)
+  next()
+})
+
+const prevHomeMiddleware = (req, res, next) => {
+  console.log('Home route accessed')
+  next()
+}
 
 app.get('/health', (req, res) => {
   res.json({ status: 'OK' })
