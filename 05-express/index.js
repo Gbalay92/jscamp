60f4d849b5b3d0fdb1df0956bf5a46e3884bbb76
Gonzalo Balay Losada <gbalay@denodo.com>
2026-01-27 16:18:37 +0100

    add temporal filters

--- 05-express/index.js
+++ 05-express/index.js
@@ -7,7 +7,31 @@ const app = express()
 
 
 app.get('/get-jobs', (req, res) => {
-  res.json(jobs)
+  console.log('req.query:', req.query) // Log query parameters, in the url after '?', example: /get-jobs?location=NYC&fulltime=true
+  const { text, title, level, limit, technology, offset } = req.query
+  let filteredJobs = jobs
+  if (text) {
+    const lowerText = text.toLowerCase()
+    filteredJobs = filteredJobs.filter(job =>
+      job.title.toLowerCase().includes(lowerText) ||
+      job.company.toLowerCase().includes(lowerText) ||
+      job.description.toLowerCase().includes(lowerText)
+    )
+  } 
+  if (title) {
+    const lowerTitle = title.toLowerCase()
+    filteredJobs = filteredJobs.filter(job => 
+      job.title.toLowerCase().includes(lowerTitle)
+    )
+  }
+  if (technology) {
+    const lowerTech = technology.toLowerCase()
+    filteredJobs = filteredJobs.filter(job => 
+      job.technologies.some(tech => tech.toLowerCase() === lowerTech)
+    )
+  }
+  //etc...
+  res.json(filteredJobs)
 })
 
 app.get('/get-job/:id', (req, res) => {
