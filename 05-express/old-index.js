4e8382d69220bd39785fa752d093b60dfdde0e39
Gonzalo Balay Losada <gbalay@denodo.com>
2026-01-29 12:46:12 +0100

    add file old-index.js

---
+++ 05-express/old-index.js
@@ -0,0 +1,83 @@
+import express from 'express'
+import { jobs } from './jobs.json' with { type: 'json' }
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+
+app.get('/jobs', (req, res) => {
+  console.log('req.query:', req.query) // Log query parameters, in the url after '?', example: /get-jobs?location=NYC&fulltime=true
+  const { text, title, level, limit = 10, technology, offset = 0} = req.query
+  let filteredJobs = jobs
+  if (text) {
+    const lowerText = text.toLowerCase()
+    filteredJobs = filteredJobs.filter(job =>
+      job.title.toLowerCase().includes(lowerText) ||
+      job.company.toLowerCase().includes(lowerText) ||
+      job.description.toLowerCase().includes(lowerText)
+    )
+  } 
+  if (title) {
+    const lowerTitle = title.toLowerCase()
+    filteredJobs = filteredJobs.filter(job => 
+      job.title.toLowerCase().includes(lowerTitle)
+    )
+  }
+  if (technology) {
+    const lowerTech = technology.toLowerCase()
+    filteredJobs = filteredJobs.filter(job => 
+      job.technologies.some(tech => tech.toLowerCase() === lowerTech)
+    )
+  }
+  //etc...
+
+  limitNumber = Number(limit) || filteredJobs.length
+  offsetNumber = Number(offset) || 0
+  paginatedJobs = filteredJobs.slice(offsetNumber, offsetNumber + limitNumber)
+  res.json(paginatedJobs)
+})
+
+app.get('/jobs/:id', (req, res) => {
+  const jobId = parseInt(req.params.id, 10)
+  const job = { id: jobId, title: 'Sample Job', company: 'Sample Company' }
+  res.json(job)
+})
+
+app.delete('/jobs/:id', (req, res) => {
+  //TODO
+})
+
+app.post('/jobs', (req, res) => {
+  //TODO
+})
+
+app.put('/jobs/:id', (req, res) => {
+  //TODO: to update entire resource
+})
+
+app.patch('/jobs/:id', (req, res) => {
+  //TODO: to update partial resource
+})
+
+app.get('/', prevHomeMiddleware, (req, res) => {
+  res.send('Hello, Express!')
+})
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`)
+  next()
+})
+
+const prevHomeMiddleware = (req, res, next) => {
+  console.log('Home route accessed')
+  next()
+}
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'OK' })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
