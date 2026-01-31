e598033c45de641be3180976b77f2794e05ed6b9
Gonzalo Balay Losada <gbalay@denodo.com>
2026-01-31 12:38:56 +0100

    applying controller

--- 05-express/controllers/jobs.js
+++ 05-express/controllers/jobs.js
@@ -30,4 +30,69 @@ export class JobsController {
           paginatedJobs = filteredJobs.slice(offsetNumber, offsetNumber + limitNumber)
           return res.json({ data: paginatedJobs, total: filteredJobs.length, limit: limitNumber, offset: offsetNumber })
     }
+
+    static async getById(req, res) {
+      const jobId = parseInt(req.params.id, 10)
+        const job = jobs.find(job => job.id === jobId)
+        if (!job) {
+          return res.status(404).json({ error: 'Job not found' })
+        }
+        return res.json(job)
+    }
+
+    static async deleteById(req, res) {
+      //TODO
+    }
+
+    static async create(req, res) {
+      const { title, company, description, technologies, data } = req.body
+      const newJob = {
+        id: crypto.randomUUID(),
+        title,
+        company,
+        description,
+        technologies,
+        data
+      }
+    
+      jobs.push(newJob)
+      return res.status(201).json(newJob)
+    }
+
+    static async updateById(req, res) {
+      const jobId = req.params.id
+      const jobIndex = jobs.findIndex(job => job.id === jobId)
+      if (jobIndex === -1) {
+        return res.status(404).json({ error: 'Job not found' })
+      }
+      const { title, company, description, technologies, data } = req.body
+      jobs[jobIndex] = {
+        ...jobs[jobIndex],
+        title,
+        company,
+        description,
+        technologies,
+        data
+      }
+      return res.json(jobs[jobIndex])
+    }
+
+    static async patchById(req, res) {
+      const jobId = req.params.id
+      const jobIndex = jobs.findIndex(job => job.id === jobId)
+      if (jobIndex === -1) {
+        return res.status(404).json({ error: 'Job not found' })
+      }
+      const { title, company, description, technologies, data } = req.body
+      jobs[jobIndex] = {
+        ...jobs[jobIndex],
+        ...(title && { title }),
+        ...(company && { company }),
+        ...(description && { description }),
+        ...(technologies && { technologies }),
+        ...(data && { data })
+      }
+      return res.json(jobs[jobIndex])
+    }
+
 }
--- 05-express/routes/jobs.js
+++ 05-express/routes/jobs.js
@@ -1,75 +1,14 @@
 import { Router } from 'express'
-import crypto from 'crypto'
 import { jobs } from '../jobs.json' with { type: 'json' }
 import { JobsController } from '../controllers/jobs'
 
 export const jobsRouter = Router()
-
+jobsRouter.use(express.json()) // Middleware to parse JSON bodies
 
 //CRUD operations for /jobs endpoint
 jobsRouter.get('/', JobsController.getAll)
-
-jobsRouter.get('/:id', (req, res) => {
-  const jobId = parseInt(req.params.id, 10)
-  const job = jobs.find(job => job.id === jobId)
-  if (!job) {
-    return res.status(404).json({ error: 'Job not found' })
-  }
-  res.json(job)
-})
-
-jobsRouter.delete('/:id', (req, res) => {
-  //TODO
-})
-
-jobsRouter.use(express.json()) // Middleware to parse JSON bodies
-jobsRouter.post('/', (req, res) => {
-  const { title, company, description, technologies, data } = req.body
-  const newJob = {
-    id: crypto.randomUUID(),
-    title,
-    company,
-    description,
-    technologies,
-    data
-  }
-
-  jobs.push(newJob)
-  return res.status(201).json(newJob)
-})
-
-jobsRouter.put('/:id', (req, res) => {
-  const jobId = req.params.id
-  const jobIndex = jobs.findIndex(job => job.id === jobId)
-  if (jobIndex === -1) {
-    return res.status(404).json({ error: 'Job not found' })
-  }
-  const { title, company, description, technologies, data } = req.body
-  jobs[jobIndex] = {
-    ...jobs[jobIndex],
-    title,
-    company,
-    description,
-    technologies,
-    data
-  }
-  res.json(jobs[jobIndex])
-})
-
-jobsRouter.patch('/:id', (req, res) => {
-  const jobId = req.params.id
-  const jobIndex = jobs.findIndex(job => job.id === jobId)
-  if (jobIndex === -1) {
-    return res.status(404).json({ error: 'Job not found' })
-  }
-  const { title, company, description, technologies, data } = req.body
-  jobs[jobIndex] = {
-    ...jobs[jobIndex],
-    ...(title && { title }),
-    ...(company && { company }),
-    ...(description && { description }),
-    ...(technologies && { technologies }),
-    ...(data && { data })
-  }
-  res.json(jobs[jobIndex])
-})
+jobsRouter.get('/:id', JobsController.getById)
+jobsRouter.delete('/:id', JobsController.deleteById)
+jobsRouter.post('/', JobsController.create)
+jobsRouter.put('/:id', JobsController.updateById)
+jobsRouter.patch('/:id', JobsController.patchById)
