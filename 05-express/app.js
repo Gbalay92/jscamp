8774f5ff225fadb8fe3ac3c6442ea920e0536572
Gonzalo Balay Losada <gbalay@denodo.com>
2026-01-31 12:19:06 +0100

    extract cors logic

--- 05-express/app.js
+++ 05-express/app.js
@@ -1,20 +1,10 @@
 import express from 'express'
-import cors from 'cors'
+import { corsMiddleware } from './middlewares/cors.js'
 
 const PORT = process.env.PORT || 3000
 const app = express()
 
-//app.use(cors) // Enable CORS for all origins
-const allowedOrigins = ['http://localhost:3000', 'http://example.com']
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true) //NULL means no error
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}))
+app.use(corsMiddleware())
 app.use(express.json()) // Middleware to parse JSON bodies
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
---
+++ 05-express/middlewares/cors.js
@@ -0,0 +1,19 @@
+import cors from 'cors'
+
+//app.use(cors) // Enable CORS for all origins
+const allowedOrigins = ['http://localhost:3000', 'http://example.com']
+
+export const corsMiddleware = ({ acceptedOrigins = allowedOrigins } = {}) => {
+    return cors({
+        origin: function(origin, callback) {
+            if (!origin || acceptedOrigins.includes(origin)) {
+                return callback(null, true) //NULL means no error
+            } else {
+                return callback(new Error('Not allowed by CORS'))
+            }
+        }
+    })
+}
+
+
+app.use()
