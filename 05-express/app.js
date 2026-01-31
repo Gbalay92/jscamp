07fb6f250a6120c3df591948d3418e9bcbe8cf6c
Gonzalo Balay Losada <gbalay@denodo.com>
2026-01-31 12:24:55 +0100

    create jobsRouter

--- 05-express/app.js
+++ 05-express/app.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import { corsMiddleware } from './middlewares/cors.js'
+import { jobsRouter } from './routes/jobs.js'
 
 const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(corsMiddleware())
 app.use(express.json()) // Middleware to parse JSON bodies
+
+app.use('/jobs', jobsRouter)
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
 })
---
+++ 05-express/routes/jobs.js
@@ -0,0 +1,104 @@
+import { Router } from 'express'
+import crypto from 'crypto'
+import { jobs } from '../jobs.json' with { type: 'json' }
+
+export const jobsRouter = Router()
+
+
+//CRUD operations for /jobs endpoint
+jobsRouter.get('/', (req, res) => {
+  console.log('req.query:', req.query) // Log query parameters, in the url after '?', example: /get-jobs?location=NYC&fulltime=true
+  const { text, title, level, limit = 10, technology, offset = 0} = req.query
+  let filteredJobs = jobs
+  if (text) {
+    const lowerText = text.toLowerCase()
+    filteredJobs = filteredJobs.filter(job =>
+      job.title.toLowerCase().includes(lowerText) ||
+      job.company.toLowerCase().includes(lowerText) ||
+      job.description.toLowerCase().includes(lowerText)
+    )
+  } 
+  if (title) {
+    const lowerTitle = title.toLowerCase()
+    filteredJobs = filteredJobs.filter(job => 
+      job.title.toLowerCase().includes(lowerTitle)
+    )
+  }
+  if (technology) {
+    const lowerTech = technology.toLowerCase()
+    filteredJobs = filteredJobs.filter(job => 
+      job.technologies.some(tech => tech.toLowerCase() === lowerTech)
+    )
+  }
+  //etc...
+
+  limitNumber = Number(limit) || filteredJobs.length
+  offsetNumber = Number(offset) || 0
+  paginatedJobs = filteredJobs.slice(offsetNumber, offsetNumber + limitNumber)
+  res.json({ data: paginatedJobs, total: filteredJobs.length, limit: limitNumber, offset: offsetNumber })
+})
+
+jobsRouter.get('/:id', (req, res) => {
+  const jobId = parseInt(req.params.id, 10)
+  const job = jobs.find(job => job.id === jobId)
+  if (!job) {
+    return res.status(404).json({ error: 'Job not found' })
+  }
+  res.json(job)
+})
+
+jobsRouter.delete('/:id', (req, res) => {
+  //TODO
+})
+
+jobsRouter.use(express.json()) // Middleware to parse JSON bodies
+jobsRouter.post('/', (req, res) => {
+  const { title, company, description, technologies, data } = req.body
+  const newJob = {
+    id: crypto.randomUUID(),
+    title,
+    company,
+    description,
+    technologies,
+    data
+  }
+
+  jobs.push(newJob)
+  return res.status(201).json(newJob)
+})
+
+jobsRouter.put('/:id', (req, res) => {
+  const jobId = req.params.id
+  const jobIndex = jobs.findIndex(job => job.id === jobId)
+  if (jobIndex === -1) {
+    return res.status(404).json({ error: 'Job not found' })
+  }
+  const { title, company, description, technologies, data } = req.body
+  jobs[jobIndex] = {
+    ...jobs[jobIndex],
+    title,
+    company,
+    description,
+    technologies,
+    data
+  }
+  res.json(jobs[jobIndex])
+})
+
+jobsRouter.patch('/:id', (req, res) => {
+  const jobId = req.params.id
+  const jobIndex = jobs.findIndex(job => job.id === jobId)
+  if (jobIndex === -1) {
+    return res.status(404).json({ error: 'Job not found' })
+  }
+  const { title, company, description, technologies, data } = req.body
+  jobs[jobIndex] = {
+    ...jobs[jobIndex],
+    ...(title && { title }),
+    ...(company && { company }),
+    ...(description && { description }),
+    ...(technologies && { technologies }),
+    ...(data && { data })
+  }
+  res.json(jobs[jobIndex])
+})
