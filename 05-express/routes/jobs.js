26dd96797c36796fc5bf48a932e92096ccd18c52
Gonzalo Balay Losada <gbalay@denodo.com>
2026-02-23 17:05:06 +0100

    using middlewares

--- 05-express/routes/jobs.js
+++ 05-express/routes/jobs.js
@@ -1,14 +1,31 @@
 import { Router } from 'express'
 import { JobsController } from '../controllers/jobs.js'
 import express from 'express'
+import { validateJob, validateParcialJob } from '../schemas/jobs.js'
 
 export const jobsRouter = Router()
 jobsRouter.use(express.json()) // Middleware to parse JSON bodies
+
+function validateCreateJob(req, res, next) {
+    const result = validateJob(req.body)
+    if (!result.success) {
+        return res.status(400).json({ error: result.error })
+    }
+    next()
+}
+
+function validateUpdateJob(req, res, next) {
+    const result = validateParcialJob(req.body)
+    if (!result.success) {
+        return res.status(400).json({ error: result.error })
+    }
+    next()
+}
 
 //CRUD operations for /jobs endpoint
 jobsRouter.get('/', JobsController.getAll)
 jobsRouter.get('/:id', JobsController.getById)
 jobsRouter.delete('/:id', JobsController.deleteById)
-jobsRouter.post('/', JobsController.create)
-jobsRouter.put('/:id', JobsController.updateById)
-jobsRouter.patch('/:id', JobsController.patchById)
+jobsRouter.post('/', validateCreateJob, JobsController.create)
+jobsRouter.put('/:id', validateUpdateJob, JobsController.updateById)
+jobsRouter.patch('/:id', validateUpdateJob, JobsController.patchById)
